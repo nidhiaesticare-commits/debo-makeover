e3371f17764f8172a2e39b5fa9fdc3a818238d54
Jaydeepsinh Jadeja <iimjayy21@gmail.com>
2026-04-15 13:02:02 +0000

    Update website to latest version

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: '/debo-makeover/',
+  build: {
+    outDir: 'docs',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  },
   plugins: [react()],
 });
